f44780cbe841c6ca1fd922dcf928a343b5eadd5c
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-06 13:01:41 +0600

    зарегестрировал модель в админке

--- source/guestbook_app/admin.py
+++ source/guestbook_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from guestbook_app.models import Entry
 
 # Register your models here.
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'status', 'created_at']
+    list_filter = ['status',]
+    search_fields = ['name', 'email']
+
+
+admin.site.register(Entry, EntryAdmin)
