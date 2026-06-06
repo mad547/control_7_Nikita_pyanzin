a287abc52b45d1e1b66f6570e716a76f414cbf0f
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-06 20:08:15 +0600

    добавил миграции

--- source/guestbook_project/settings.py
+++ source/guestbook_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'guestbook.app',
+    'guestbook_app',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'r0-ru'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Asia/Bishkek'
 
