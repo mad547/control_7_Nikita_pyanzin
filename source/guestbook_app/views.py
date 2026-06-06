41e5f74ab5fe646c8888817266976be95c35b777
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-06 14:04:26 +0600

    добавил представление entry_add

--- source/guestbook_app/views.py
+++ source/guestbook_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from guestbook_app.models import Entry
+from guestbook_app.forms import EntryForm
 
-# Create your views here.
+
+def entry_list(request):
+    entries = Entry.objects.filter(status='active')
+    context = {'entries': entries}
+    return render(request, 'guestbook_app/entry_list.html', context)
+
+
+def entry_add(request):
+    form = EntryForm()
+    if request.method == 'GET':
+        return render(request, 'guestbook_app/entry_add.html', {'form': form})
+    elif request.method == 'POST':
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('entry_list')
+        return render(request, 'guestbook_app/entry_add.html', {'form': form})
